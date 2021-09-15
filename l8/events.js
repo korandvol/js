6f760205198bcc0f8362ef8bbb6c2c488daf6026
korandvol <korandvol@gmail.com>
2021-09-15 12:58:25 +0300

    events

---
+++ l8/events.js
@@ -0,0 +1,16 @@
+'use strict'
+function testConnection(){
+	alert('Все гут!)');
+	console.log('All good!');
+}
+
+function textHide(){
+	var divEl = document.getElementById('text');
+	text.innerHTML = "";
+
+}
+
+function buttonHide(){
+	var button = document.getElementById('button').style.visibility="hidden";
+	
+}
